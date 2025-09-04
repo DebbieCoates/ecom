7df23c10787a30654107a24c260738d3a33c72a7
DebbieCoates <debbie.coates@hotmail.co.uk>
2025-09-04 12:49:22 +0100

    Template Out Website

--- store/views.py
+++ store/views.py
@@ -5,3 +5,6 @@ from .models import Category, Product, Order, Customer
 def home(request):
     products = Product.objects.all()
     return render(request, 'home.html', {'products': products})
+
+def about(request):
+    return render(request, 'about.html', {})
