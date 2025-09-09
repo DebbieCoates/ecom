90c3c1ee8eb587494f48cbdb6ac01a503df0d048
DebbieCoates <debbie.coates@hotmail.co.uk>
2025-09-09 19:06:57 +0100

    Update User Profile (22)

--- store/views.py
+++ store/views.py
@@ -6,7 +6,22 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm  
 from django import forms 
-from .forms import SignUpForm
+from .forms import SignUpForm, UpdateUsrForm
+
+
+def update_user(request):
+    if request.user.is_authenticated:
+        current_user = User.objects.get(id=request.user.id)
+        user_form = UpdateUsrForm(request.POST or None, instance=current_user)
+        if user_form.is_valid():
+            user_form.save()
+            login(request, current_user)
+            messages.success(request, "Profile updated successfully!")
+            return redirect('home')
+        return render(request, 'update_user.html', {'user_form': user_form})
+    else:
+        messages.success(request, "You must be logged in to access that page!")
+        return redirect('home')
 
 
 def category_summary(request):
