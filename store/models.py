59a9739072d03990faf85ee29fd10242d7f8c782
DebbieCoates <debbie.coates@hotmail.co.uk>
2025-09-09 20:21:52 +0100

    Extend User Profiles (24)

--- store/models.py
+++ store/models.py
@@ -1,6 +1,33 @@
 from django.db import models
 import datetime
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
+
+
+# Create Customer Profile
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_modified = models.DateTimeField(auto_now=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    address1 = models.CharField(max_length=255, blank=True, null=True)
+    address2 = models.CharField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=255, blank=True, null=True)
+    county = models.CharField(max_length=255, blank=True, null=True)
+    postcode = models.CharField(max_length=255, blank=True, null=True)
+    country = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+# Create a user profile by default when a user signs up
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+
+# Automate the profile creation
+post_save.connect(create_profile, sender=User)
 
 # Categories of Products
 class Category(models.Model):
