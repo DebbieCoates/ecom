c1e24bea25caa9d54d06e7902e615407204c3650
DebbieCoates <debbie.coates@hotmail.co.uk>
2025-09-11 11:56:17 +0100

    Order Checkout (32)

--- payment/views.py
+++ payment/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
 from cart.cart import Cart
 
+from payment.forms import ShippingAddressForm
+from payment.models import shippingAddress
+
 # Create your views here.
 
 def checkout(request):
-    	# Get the cart
+	# Get the cart
 	cart = Cart(request)
 	cart_products = cart.get_prods
 	quantities = cart.get_quants
 	totals = cart.cart_total()
-	return render(request, "payment/checkout.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals})
+
+	if request.user.is_authenticated:
+		#checkout as logged in user
+        #shipping user
+		shipping_user = shippingAddress.objects.get(user__id=request.user.id)
+        #Shipping form
+		shipping_form = ShippingAddressForm(request.POST or None, instance=shipping_user)
+		return render(request, "payment/checkout.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_form":shipping_form})
+	else:
+		#checkout as guest
+		shipping_form = ShippingAddressForm(request.POST or None)
+		return render(request, "payment/checkout.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_form":shipping_form})
 
 
 
